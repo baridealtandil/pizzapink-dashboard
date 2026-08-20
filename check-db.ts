68bafb87febac680e2db61e10bd746cb82c9bae5
baridealtandil <baridealtandil@gmail.com>
2026-08-20 15:03:28 -0300

    Remove unused scratch files

--- check-db.ts
+++
@@ -1,7 +0,0 @@
-import { query } from "./src/db";
-async function run() {
-  console.log(await query("SHOW INDEXES FROM mxite;"));
-  console.log(await query("EXPLAIN SELECT SUM(total) FROM mxfac WHERE fecha = '2026-07-11';"));
-  process.exit(0);
-}
-run();
